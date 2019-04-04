773eb2b665ff0e8c6ab7cd843466d6d0b47758f6
Henri Petrow <henri.petrow@lu.fi>
2019-04-04 17:31:51 +0300

    Improved short circuit test.

--- scripts/find_from_database.py
+++ scripts/find_from_database.py
@@ -13,6 +13,12 @@ database = DatabaseInterfaceBrowse()
 hybrid_list = database.list_hybrids_by_lot(1)
 for hybrid in hybrid_list:
     production_data_int = database.get_production_results(hybrid)
+    production_data = []
+    for item in production_data_int:
+        if item is None or item == "None":
+            production_data.append("")
+        else:
+            production_data.append(item)
     if production_data[29] == 'red':
         red_list.append(hybrid[6:])
     elif production_data[29] == 'yellow':
