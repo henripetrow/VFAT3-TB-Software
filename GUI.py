66207710babf344d84e5c6ae978a8f5b67481d51
Henri Petrow <henri.petrow@lu.fi>
2019-06-07 13:38:41 +0300

    s-bit check

--- GUI.py
+++ GUI.py
@@ -1965,8 +1965,8 @@ class VFAT3_GUI:
         self.register[0xffff].RUN[0] = 0
         self.write_register(0xffff)
 
-        #if production == "yes":
-        #    self.database.save_sbit_errors(''.join(str(e) for e in errors))
+        if production == "yes":
+            self.database.save_sbit_errors(''.join(str(e) for e in errors))
 
         error = self.check_selection_criteria(errors.count(1), lim_sbits, "S-bit")
         stop_time = time.time()
