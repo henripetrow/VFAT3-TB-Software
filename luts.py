38fe2e5263ee84407610cd9fd75fa4ebafa0ee5a
henripetrow <henri_petrow@hotmail.com>
2019-06-13 13:51:17 +0300

    sbit routine

--- luts.py
+++ luts.py
@@ -55,7 +55,7 @@ lim_sbits = [0, 0, 0, 0]
 
 # selection criteria for S-curve channel classification.
 
-lim_enc_noisy_channel = 0.7
+lim_enc_noisy_channel = 2.5
 lim_enc_unbonded_channel = 0.15
 
 # Selection criteria
