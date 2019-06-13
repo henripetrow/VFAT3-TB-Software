9e3af13e319f5d9f2f07b18f367b560bb45cba3f
henripetrow <henri_petrow@hotmail.com>
2019-06-13 13:28:23 +0300

    sbit routine

--- routines/routines.py
+++ routines/routines.py
@@ -855,7 +855,7 @@ def fit_scurve(charge_data, hit_data):
     print hit_data
     st_x = 2
     st_y = 0.5
-    params, params_covariance = curve_fit(fit_func, charge_data, hit_data, p0=[st_y, st_x])
+    params, params_covariance = curve_fit(fit_func, charge_data, hit_data, p0=[st_x, st_y])
     print "Optimal parameters:"
     print params
     plt.figure()
