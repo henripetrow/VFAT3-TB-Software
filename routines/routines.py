c79c2f9138c04b49b33a26338a167158a0289e34
henripetrow <henri_petrow@hotmail.com>
2019-06-13 15:32:57 +0300

    sbit routine

--- routines/routines.py
+++ routines/routines.py
@@ -855,7 +855,9 @@ def fit_func(x, a, b):
 def fit_scurve(charge_data, hit_data):
     print "Fitting function."
     print charge_data
+    hit_data[:] = [x / 100 for x in hit_data]
     print hit_data
+
     np_x = np.array(charge_data)
     np_y = np.array(hit_data)
     st_x = 1
