17f1e57e7b52af6d77927bb922a0709566890ddc
Henri Petrow <henri_petrow@hotmail.com>
2019-06-14 12:30:24 +0300

    testing trimDAC

--- routines/routines.py
+++ routines/routines.py
@@ -753,12 +753,12 @@ def scurve_analyze_old(obj, dac_values, channels, scurve_data, folder=""):
     # plt.grid(True)
     # plt.show()
 
-    plt.hist(mean_list, bins=100)
-    plt.title('Threshold spread')
-    plt.xlabel('Thr [fC]')
-    plt.ylabel('#')
-    plt.grid()
-    plt.show()
+    # plt.hist(mean_list, bins=100)
+    # plt.title('Threshold spread')
+    # plt.xlabel('Thr [fC]')
+    # plt.ylabel('#')
+    # plt.grid()
+    # plt.show()
 
 
     fig, ax = plt.subplots()
