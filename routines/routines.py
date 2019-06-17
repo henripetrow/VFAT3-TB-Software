dc6e0a9dccd21266b5200d95fcdef04110cf581b
henripetrow <henri_petrow@hotmail.com>
2019-06-18 00:22:38 +0300

    sbit routine

--- routines/routines.py
+++ routines/routines.py
@@ -121,6 +121,11 @@ def scurve_all_ch_execute(obj, scan_name, arm_dac=100, ch=[0, 127], ch_step=1, c
             obj.register[135].ARM_DAC[0] = arm_dac
             obj.write_register(135)
 
+            for k in range(0, 128):
+                obj.register[k].mask[0] = 0
+                obj.write_register(k)
+                time.sleep(0.5)
+
             #obj.register[137].LAT[0] = latency
             #obj.write_register(137)
 
