6af85ca332b3e2587b3d43914d665e216866be1d
Henri Petrow <henri_petrow@hotmail.com>
2019-06-17 13:49:43 +0300

    testing trimDAC

--- FW_interface.py
+++ FW_interface.py
@@ -234,7 +234,7 @@ class FW_interface:
                            len(cal_dac_array)]
                 out = self.execute_req(message, no_packets=1, timeout=30, scurve="yes")
                 time.sleep(0.5)
-                output.insert(i, out)
+                output.insert(i-1, out)
 
         return output
 
