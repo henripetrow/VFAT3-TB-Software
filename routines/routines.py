ac051ac2fe981103b61ec84fb62567cffabf0a93
Henri Petrow <henri_petrow@hotmail.com>
2019-06-17 14:02:02 +0300

    testing trimDAC

--- routines/routines.py
+++ routines/routines.py
@@ -703,7 +703,8 @@ def scurve_analyze_old(obj, dac_values, channels, scurve_data, folder=""):
 
     for i, channel in enumerate(channels):
         data = scurve_data[i]
-        print data
+        if isinstance(data, (list,)):
+            data = data[0]
         if all(v == 0 for v in data):
             print "Dead channel."
             dead_channels.append(channel)
