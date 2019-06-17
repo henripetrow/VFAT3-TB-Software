fcd4371c2dd682d8b9cee79b7012bf5e14bc948c
Henri Petrow <henri_petrow@hotmail.com>
2019-06-17 14:25:16 +0300

    testing trimDAC

--- FW_interface.py
+++ FW_interface.py
@@ -233,7 +233,7 @@ class FW_interface:
                            triggers & 0xFF, arm_dac, delay, d1 >> 8, d1 & 0xFF, d2 >> 8, d2 & 0xFF, 1,
                            len(cal_dac_array)]
                 out = self.execute_req(message, no_packets=1, timeout=30, scurve="yes")
-                print out
+                out = out[0]
                 if all(v == 0 for v in out):
                     print "Detected zero output in channel %s." % i
                     print "Trying to re-run s-curve for it."
