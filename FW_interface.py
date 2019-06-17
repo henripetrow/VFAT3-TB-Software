c575e4a3150b0093baa6c5ef47424bcf5a5acfca
Henri Petrow <henri_petrow@hotmail.com>
2019-06-17 13:56:24 +0300

    testing trimDAC

--- FW_interface.py
+++ FW_interface.py
@@ -233,6 +233,7 @@ class FW_interface:
                            triggers & 0xFF, arm_dac, delay, d1 >> 8, d1 & 0xFF, d2 >> 8, d2 & 0xFF, 1,
                            len(cal_dac_array)]
                 out = self.execute_req(message, no_packets=1, timeout=30, scurve="yes")
+                print out
                 time.sleep(0.5)
                 output.pop(i)
                 output.insert(i, out)
