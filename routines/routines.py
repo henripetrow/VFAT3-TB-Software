83300f0975d0e521e7f4173b92058a7547f47931
henripetrow <henri_petrow@hotmail.com>
2019-06-17 18:35:30 +0300

    sbit routine

--- routines/routines.py
+++ routines/routines.py
@@ -742,8 +742,8 @@ def scurve_analyze_old(obj, dac_values, channels, scurve_data, folder=""):
             r_squared = calculate_r2_score(np_x, np_y, params)
             print "R^2: %s" % r_squared
             print params
-            rms_loop_mean.append(params[0])
-            mean_loop_mean.append(params[1])
+            rms_loop_mean.append(params[1])
+            mean_loop_mean.append(params[0])
             rms = numpy.mean(rms_loop_mean)
             print rms
             mean = numpy.mean(mean_loop_mean)
