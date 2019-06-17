d8021718d2782f7d9cdbec2830d51972457d48be
henripetrow <henri_petrow@hotmail.com>
2019-06-18 00:13:48 +0300

    sbit routine

--- FW_interface.py
+++ FW_interface.py
@@ -231,7 +231,8 @@ class FW_interface:
                     d1 = 58
                     print k
                     obj.register[i].mask[0] = 0
-                    obj.write_register()
+                    obj.write_register(i)
+                    time.sleep(1)
                     message = [0xca, 0, 0x08, i-1, i+1, step_ch, cal_dac_start, cal_dac_stop, 1, latency >> 8, latency & 0xFF,
                                triggers >> 8, triggers & 0xFF, arm_dac, delay, d1 >> 8, d1 & 0xFF, d2 >> 8, d2 & 0xFF]
 
