a74aed49733512c50a5d4919df28a330dc074652
henripetrow <henri_petrow@hotmail.com>
2019-06-19 16:41:33 +0300

    improving xml generation

--- luts.py
+++ luts.py
@@ -55,7 +55,7 @@ lim_sbits = [0, 0, 0, 0]
 
 # selection criteria for S-curve channel classification.
 
-lim_enc_noisy_channel = 1
+lim_enc_noisy_channel = 1.5
 lim_enc_noisy_channel_flex_end_channels = 4
 lim_enc_unbonded_channel = 0.13
 
