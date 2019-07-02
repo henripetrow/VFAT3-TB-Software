e7948e464604ff6d0fd6e90d8619b601e7dbc4de
henripetrow <henri_petrow@hotmail.com>
2019-07-02 20:32:43 +0300

    Added columns Iref_mv and Modified for the database.

--- scripts/generate_xml_from_database.py
+++ scripts/generate_xml_from_database.py
@@ -73,7 +73,7 @@ temp_list = []
 
 for h in hybrid_list:
     hybrid_number = int(h[6:])
-    if hybrid_number < 6100:
+    if hybrid_number > 0:
         temp_list.append(int(h[6:]))
 
 temp_list.sort()
