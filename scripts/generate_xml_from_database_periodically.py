a7940ad1e978e09a1bb47dcafa8564add02ee1b7
henripetrow <henri_petrow@hotmail.com>
2019-07-20 00:26:38 +0300

    Periodical xml creation.

--- scripts/generate_xml_from_database_periodically.py
+++ scripts/generate_xml_from_database_periodically.py
@@ -204,7 +204,7 @@ if len(hybrid_list) > 0:
     localfile2 = "../results/xml/VFAT3_Production_summary.xml"
     remotehost = "gem-machine-a"
     remotefile = "/home/dbspool/spool/gem/int2r/"
-    #remotefile = "~/testaus/"
+    remotefile = "~/testaus/"
     print subprocess.Popen(['scp', '%s' % localfile1, '%s:%s' % (remotehost, remotefile)], stdout=subprocess.PIPE).communicate()
     time.sleep(300)
     print subprocess.Popen(['scp', '%s' % localfile2, '%s:%s' % (remotehost, remotefile)], stdout=subprocess.PIPE).communicate()
