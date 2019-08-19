e94cf060da90195fe34a1914a1d836f8eb18f04c
henripetrow <henri_petrow@hotmail.com>
2019-08-19 11:24:46 +0300

    Periodical xml creation.

--- scripts/generate_xml_from_database_periodically.py
+++ scripts/generate_xml_from_database_periodically.py
@@ -24,8 +24,8 @@ with open('./gem_db_info.dat', 'r') as f:
 nr_of_days = 1
 
 database = DatabaseInterfaceBrowse()
-#hybrid_list = database.list_hybrids_modified_in_days(int(nr_of_days))
-hybrid_list = [9023]
+hybrid_list = database.list_hybrids_modified_in_days(int(nr_of_days))
+hybrid_list = [9022]
 print "Listing hybrids from the database."
 test_hybrids = []
 temp_hybrid_list = []
