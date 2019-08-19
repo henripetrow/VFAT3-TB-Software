43bbce9aae3b7bca8e7b419f298d5ae2e8c29eb1
henripetrow <henri_petrow@hotmail.com>
2019-08-19 18:01:56 +0300

    Periodical xml creation.

--- scripts/DatabaseInterfaceBrowse.py
+++ scripts/DatabaseInterfaceBrowse.py
@@ -47,7 +47,7 @@ class DatabaseInterfaceBrowse:
             date = d.strftime("%d%m%Y")
             query += "Modified='%s'" % date
         query += ";"
-        query = "SELECT * FROM Production WHERE Modified='06082019';"
+        query = "SELECT * FROM Production WHERE Modified='05082019';"
         print query
         self.open_connection()
         self.cursor.execute(query)
