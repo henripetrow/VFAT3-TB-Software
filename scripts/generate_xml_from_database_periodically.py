c18e665d1fca10dba9da9ba46120acb82f906f4f
henripetrow <henri_petrow@hotmail.com>
2019-09-04 14:42:56 +0300

    fixed xml generation

--- scripts/generate_xml_from_database_periodically.py
+++ scripts/generate_xml_from_database_periodically.py
@@ -25,7 +25,7 @@ nr_of_days = 1
 
 database = DatabaseInterfaceBrowse()
 hybrid_list = database.list_hybrids_modified_in_days(int(nr_of_days))
-hybrid_list = database.list_hybrids_modified_by_day('19082019')
+hybrid_list = database.list_hybrids_modified_by_day('29082019')
 #hybrid_list = [9022]
 print "Listing hybrids from the database."
 test_hybrids = []
