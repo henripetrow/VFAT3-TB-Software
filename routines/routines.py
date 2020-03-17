3625b920ae48ee5f80d3d0efdb1351dc5c853bcf
Henri Petrow <henri_petrow@hotmail.com>
2020-03-17 11:07:50 +0200

    Threshold scan

--- routines/routines.py
+++ routines/routines.py
@@ -32,10 +32,10 @@ def find_threshold(obj):
     # dac_start = [254, 240, 225]
     # dac_stop = [120, 90, 10]
     # gains = ['High', 'Medium', 'Low']
-    arm_dac_stop = [201]
-    dac_start = [254]
-    dac_stop = [120]
-    gains = ['High']
+    arm_dac_stop = [141]
+    dac_start = [225]
+    dac_stop = [10]
+    gains = ['Low']
     for j, gain in enumerate(gains):
         for arm_dac in range(30, arm_dac_stop[j], 20):
             print "ARM_DAC: %s" % arm_dac
