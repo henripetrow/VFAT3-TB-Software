818d65c66e36a1e6b03596bac7cdf32a9f70c223
Henri Petrow <henri_petrow@hotmail.com>
2020-03-20 10:43:36 +0200

    Threshold scan

--- scripts/threshold_plot.py
+++ scripts/threshold_plot.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+from scipy import stats
 
 gain = 'High'
 arm_values = [30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
