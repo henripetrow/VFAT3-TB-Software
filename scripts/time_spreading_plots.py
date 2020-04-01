f7b4d15c2e78952eb3201f2d859455b9737b7f16
Henri Petrow <henri_petrow@hotmail.com>
2020-04-01 13:48:44 +0300

    Threshold scan

---
+++ scripts/time_spreading_plots.py
@@ -0,0 +1,11 @@
+import numpy
+import matplotlib.pyplot as plt
+
+timestamp = "010420201143"
+folder = "../../cernbox/VFAT3_charge_distribution/Data/run_%s/" % timestamp
+data_file = "%s%soutput_data.dat" % (folder, timestamp)
+
+import data_file
+
+
+print Low_data_ps0_lat2
