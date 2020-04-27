4d7cfccb2dc4c9ebdfbb686fbafd7cfcfa95dcc1
Henri Petrow <henri_petrow@hotmail.com>
2020-04-27 14:01:54 +0300

    Threshold scan

--- routines/routines.py
+++ routines/routines.py
@@ -1051,7 +1051,7 @@ def measure_charge_distribution(obj):
     #gains = ['High', 'Medium', 'Low']
     gains = ['High']
     #dynamic_range = {'High': 9.5, 'Medium': 28, 'Low': 55}
-    dynamic_range = {'High': 6, 'Medium': 55, 'Low': 55}
+    dynamic_range = {'High': 3, 'Medium': 55, 'Low': 55}
     arm_dac_fcM = {'Low': 0.308756078585, 'Medium': 0.160574730846, 'High': 0.0525736788946}
     arm_dac_fcB = {'Low': -0.20026469513, 'Medium': -0.344217476814, 'High': -0.225712925757}
 
