8c0cb3038bab6a326300d0ca9a5ad98880b1a43d
Henri Petrow <henri_petrow@hotmail.com>
2020-05-04 10:41:28 +0300

    Threshold scan

--- routines/independent_scurve.py
+++ routines/independent_scurve.py
@@ -30,6 +30,7 @@ stop_dac_value = 254
 arm_dac = 100
 triggers = 10
 latency = 50
+verbose = 'yes'
 print "Running S-curves for channels: %i-%i, for CAL_DAC range: %i-%i:" % (start_ch, stop_ch, start_dac_value, stop_dac_value)
 samples_per_dac_value = 100
 
