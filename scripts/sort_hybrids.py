bf7ddebad3f2544931cfceb9678b7b075e574e83
Henri Petrow <henri_petrow@hotmail.com>
2020-06-30 14:22:47 +0300

    Production statistics

--- scripts/sort_hybrids.py
+++ scripts/sort_hybrids.py
@@ -7,9 +7,9 @@ from scripts.DatabaseInterfaceBrowse import *
 database = DatabaseInterfaceBrowse()
 hybrid_list = database.list_hybrids_modified_by_state('red', greater=6100, smaller=50000)
 i = 0
-cal_dac_list = [8636, 7334, 7330, 7111, 6820]
+cal_dac_list = [8636, 7334, 6820]
 buffer_offset_list = [9985, 9953, 9827, 7463]
-adc_list = [6105, 6560, 6631, 6632, 6810, 7460, 10371, 10393, 7439, 7042, 6641, 6448]
+adc_list = [6105, 6560, 6631, 6632, 6810, 7460, 10371, 10393, 7439, 7042, 6641, 6448, 7330, 7111]
 
 noise_list = [7925, 7843, 7731, 7206, 6900, 6555, 6445, 6130] #weird. noise?
 
@@ -22,6 +22,7 @@ for hybrid in hybrid_list:
             production_data.append(0)
         else:
             production_data.append(item)
+
     print_text = "Hybrid: %s" % hybrid
     if int(production_data[1]) == 0:
         print_text += ", short circuit1"
