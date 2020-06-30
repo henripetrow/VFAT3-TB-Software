b7d0ce6ada988201c65af9f3dbf50168362bc27b
Henri Petrow <henri_petrow@hotmail.com>
2020-06-30 13:32:40 +0300

    Threshold scan

--- scripts/sort_hybrids.py
+++ scripts/sort_hybrids.py
@@ -9,7 +9,7 @@ hybrid_list = database.list_hybrids_modified_by_state('red', greater=6100, small
 i = 0
 cal_dac_list = [10371, 10393, 9985, 9953, 9827, 8636, 7463, 7460, 7439, 7334, 7330, 7111, 7042, 6820, 6810, 6641, 6632,
                 6631, 6560, 6448, 6105] #Vai ADC0 ongelma?
-others_list = [7925, 7731, 7206, 6900, 6555, 6445, 6130] #weird. noise?
+others_list = [7925, 7843, 7731, 7206, 6900, 6555, 6445, 6130] #weird. noise?
 old_syc_sc = [7393]
 for hybrid in hybrid_list:
     production_data_int = database.get_production_results(hybrid)
@@ -35,3 +35,13 @@ for hybrid in hybrid_list:
     i += 1
     print print_text
 print i
+
+for hybrid in cal_dac_list_list:
+    production_data_int = database.get_production_results(hybrid)
+    production_data = []
+    for item in production_data_int:
+        if item is None or item == "None":
+            production_data.append(0)
+        else:
+            production_data.append(item)
+    print_text = "Hybrid: %s, %s, %s, %s, %s" % (hybrid, production_data[5], production_data[6], production_data[7], production_data[8])
