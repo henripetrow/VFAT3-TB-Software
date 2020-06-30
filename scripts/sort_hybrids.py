c459103731e23cbccd585ad32b985e3ef7d65201
Henri Petrow <henri_petrow@hotmail.com>
2020-06-30 14:28:24 +0300

    Production statistics

--- scripts/sort_hybrids.py
+++ scripts/sort_hybrids.py
@@ -10,10 +10,11 @@ i = 0
 cal_dac_list = [8636, 7334, 6820]
 buffer_offset_list = [9985, 9953, 9827, 7463]
 adc_list = [6105, 6560, 6631, 6632, 6810, 7460, 10371, 10393, 7439, 7042, 6641, 6448, 7330, 7111]
+old_syc_sc = [7393]
 
 noise_list = [7925, 7843, 7731, 7206, 6900, 6555, 6445, 6130] #weird. noise?
 
-old_syc_sc = [7393]
+
 for hybrid in hybrid_list:
     production_data_int = database.get_production_results(hybrid)
     production_data = []
@@ -28,7 +29,7 @@ for hybrid in hybrid_list:
         print_text += ", short circuit1"
     if int(production_data[40]) == 1:
         print_text += ", short circuit"
-    if int(production_data[41]) == 1:
+    if int(production_data[41]) == 1 or hybrid in old_syc_sc:
         print_text += ", sync problem"
     if int(production_data[36]) > 0:
         print_text += ", S-bit problem"
