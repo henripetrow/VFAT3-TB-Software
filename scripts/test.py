db5f332bfe398e09730a97db4a6cea08eac0c0fc
Henri Petrow <henri_petrow@hotmail.com>
2020-07-07 12:40:18 +0300

    Production statistics

--- scripts/test.py
+++ scripts/test.py
@@ -8,9 +8,10 @@ chip_id_bin = dec_to_bin_with_stuffing(chip_id, 32)
 print chip_id
 print "Binary:"
 print chip_id_bin
-
+print ''.join(map(str, chip_id_bin))
 rm = reedmuller.ReedMuller(2, 5)
 chip_id_bin_rm = rm.encode(chip_id_bin[-16:])
 chip_id = int(''.join(map(str, chip_id_bin_rm)), 2)
 print "Reed-Muller encoded:"
+print chip_id_bin_rm
 print ''.join(map(str, chip_id_bin_rm))
