0657fa8076adf8be6fd2f140b766cee143994c5b
Henri Petrow <henri_petrow@hotmail.com>
2020-07-07 12:42:12 +0300

    Production statistics

--- scripts/test.py
+++ scripts/test.py
@@ -3,15 +3,15 @@ sys.path.append('../')
 from reedmuller import *
 from test_system_functions import dec_to_bin_with_stuffing
 
-chip_id = 6000
-chip_id_bin = dec_to_bin_with_stuffing(chip_id, 32)
-print chip_id
-print "Binary:"
-print chip_id_bin
-print ''.join(map(str, chip_id_bin))
-rm = reedmuller.ReedMuller(2, 5)
-chip_id_bin_rm = rm.encode(chip_id_bin[-16:])
-chip_id = int(''.join(map(str, chip_id_bin_rm)), 2)
-print "Reed-Muller encoded:"
-print chip_id_bin_rm
-print ''.join(map(str, chip_id_bin_rm))
+
+for chip_id in range(6000,6010):
+    chip_id_bin = dec_to_bin_with_stuffing(chip_id, 32)
+    print "Dec:"
+    print chip_id
+    print "Binary:"
+    print ''.join(map(str, chip_id_bin))
+    rm = reedmuller.ReedMuller(2, 5)
+    chip_id_bin_rm = rm.encode(chip_id_bin[-16:])
+    chip_id = int(''.join(map(str, chip_id_bin_rm)), 2)
+    print "Reed-Muller encoded:"
+    print ''.join(map(str, chip_id_bin_rm))
