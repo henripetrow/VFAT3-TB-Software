ebb2acae1714eb3b8ee46ebdef25be83ceeea70e
Henri Petrow <henri_petrow@hotmail.com>
2020-07-09 12:04:32 +0300

    Production statistics

--- scripts/find_values_for_list.py
+++ scripts/find_values_for_list.py
@@ -13,15 +13,15 @@ input_file = 'hybrids_list.txt'
 output_file = "hybrid_list_calibration_values.txt"
 
 
-# print "Reading list of hybrids from the file: %s" % input_file
-# with open(input_file, 'r') as f:
-#     hybrids = []
-#     for line in f:
-#         line.strip('\n')
-#         hybrids.append('Hybrid%s' % int(line))
+print "Reading list of hybrids from the file: %s" % input_file
+with open(input_file, 'r') as f:
+    hybrids = []
+    for line in f:
+        line.strip('\n')
+        hybrids.append('Hybrid%s' % int(line))
 
 database_browse = DatabaseInterfaceBrowse()
-hybrids = database_browse.list_hybrids()
+#hybrids = database_browse.list_hybrids()
 text = "Hybrid Iref CAL_DACm CAL_DACb ADC0M ADC0B ADC1M ADC1B\n"
 for hybrid in hybrids:
     print hybrid
